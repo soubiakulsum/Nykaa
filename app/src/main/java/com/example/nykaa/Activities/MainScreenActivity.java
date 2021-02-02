00514b7bca3863221bab9757f6b7c2f722fdc875
soubiakulsum <soubiakulsum@gmail.com>
2021-02-02 13:25:29 +0530

    Packages

---
+++ app/src/main/java/com/example/nykaa/Activities/MainScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.nykaa.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nykaa.R;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
